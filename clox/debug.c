c2acb922f0866726befb6fc0cb0cf320cc423bce
Adam Bruce <adam@adambruce.net>
2022-07-04 16:38:18 +0100

    Added comments to static methods

--- clox/debug.c
+++ clox/debug.c
@@ -3,19 +3,43 @@
 #include "debug.h"
 #include "value.h"
 
-void disassembleChunk(Chunk *chunk, const char *name) {
-    printf("== %s ==\n", name);
-    for (int offset = 0; offset < chunk->count;) {
-        offset = disassembleInstruction(chunk, offset);
-    }
-}
+/* ===== Static functions ===== */
 
+/**
+ * Prints information about a constant.
+ * @param name the name of the instruction.
+ * @param chunk the chunk that contains this instruction.
+ * @param offset the offset of the instruction within the chunk.
+ * @return the offset of the next instruction.
+ */
 static int constantInstruction(const char *name, Chunk *chunk, int offset) {
     uint8_t constant = chunk->code[offset + 1];
     printf("%-16s %4d '", name, constant);
     printValue(chunk->constants.values[constant]);
     printf("'\n");
     return offset + 2;
+}
+
+/**
+ * Prints information about a simple instruction with no operands.
+ * @param name the name of the instruction.
+ * @param offset the offset of the instruction within the chunk.
+ * @return the offset of the next instruction.
+ * @return the offset of the next instruction.
+ */
+static int simpleInstruction(const char *name, int offset) {
+    printf("%s\n", name);
+    return offset + 1;
+}
+
+/* ===== End static functions ===== */
+
+
+void disassembleChunk(Chunk *chunk, const char *name) {
+    printf("== %s ==\n", name);
+    for (int offset = 0; offset < chunk->count;) {
+        offset = disassembleInstruction(chunk, offset);
+    }
 }
 
 int disassembleInstruction(Chunk *chunk, int offset) {
@@ -39,8 +63,3 @@ int disassembleInstruction(Chunk *chunk, int offset) {
 }
 
 
-
-int simpleInstruction(const char *name, int offset) {
-    printf("%s\n", name);
-    return offset + 1;
-}
