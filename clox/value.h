2bcf24301c92efe4b1d1347da176af621ce08298
Adam Bruce <adam@adambruce.net>
2022-07-08 13:37:29 +0100

    Chapter 18: Types of Values

--- clox/value.h
+++ clox/value.h
@@ -3,7 +3,37 @@
 
 #include "common.h"
 
-typedef double Value;
+/**
+ * Lox value types.
+ */
+typedef enum {
+    VAL_BOOL,
+    VAL_NIL,
+    VAL_NUMBER
+} ValueType;
+
+/**
+ * Lox value struct.
+ * This struct holds a type and value.
+ */
+typedef struct {
+    ValueType type;
+    union {
+        bool boolean;
+        double number;
+    } as;
+} Value;
+
+#define IS_BOOL(value)    ((value).type == VAL_BOOL)
+#define IS_NIL(value)     ((value).type == VAL_NIL)
+#define IS_NUMBER(value)  ((value).type == VAL_NUMBER)
+
+#define AS_BOOL(value)    ((value).as.boolean)
+#define AS_NUMBER(value)  ((value).as.number)
+
+#define BOOL_VAL(value)   ((Value){VAL_BOOL,   {.boolean = value}})
+#define NIL_VAL           ((Value){VAL_NIL,    {.number = 0}})
+#define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
 
 /**
  * Represents the values associated with the current chunk.
@@ -19,6 +49,14 @@ typedef struct {
     /**  The array of values. */
     Value *values;
 } ValueArray;
+
+/**
+ * Compares two values, returning true of the values are equal.
+ * @param a the first value.
+ * @param b the second value.
+ * @return true if both values are equal.
+ */
+bool valuesEqual(Value a, Value b);
 
 /**
  * Initializes a value array.
--- clox/vm.c
+++ clox/vm.c
@@ -1,5 +1,6 @@
 
 #include <stdio.h>
+#include <stdarg.h>
 #include "vm.h"
 #include "debug.h"
 #include "compiler.h"
@@ -9,17 +10,64 @@ VM vm;
 /* ===== Static functions ===== */
 
 /**
+ * Returns the next value in the stack at the given distance.
+ * @param distance the distance.
+ * @return the next value.
+ */
+static Value peek(int distance) {
+    return vm.stackTop[-1 - distance];
+}
+
+/**
+ * Determines whether a value should evaluate to false.
+ * @param value the value.
+ * @return true if the value evaluates to false.
+ */
+static bool isFalsey(Value value) {
+    return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+
+/**
+ * Resets the stack.
+ */
+static void resetStack() {
+    vm.stackTop = vm.stack;
+}
+
+/**
+ * Displays a runtime error and resets the stack.
+ * @param format
+ * @param ...
+ */
+static void runtimeError(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    vfprintf(stderr, format, args);
+    va_end(args);
+    fputs("\n", stderr);
+
+    size_t instruction = vm.ip - vm.chunk->code - 1;
+    int line = vm.chunk->lines[instruction];
+    fprintf(stderr, "[line %d] in script\n", line);
+    resetStack();
+}
+
+/**
  * Instructs the virtual machine to start interpreting.
  * @return the result.
  */
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
-#define BINARY_OP(op)     \
-    do {                  \
-        double b = pop(); \
-        double a = pop(); \
-        push(a op b);     \
+#define BINARY_OP(valueType, op)                          \
+    do {                                                  \
+        if (!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) { \
+            runtimeError("Operands must be numbers.");    \
+            return INTERPRET_RUNTIME_ERROR;               \
+        }                                                 \
+        double b = AS_NUMBER(pop());                      \
+        double a = AS_NUMBER(pop());                      \
+        push(valueType(a op b));                          \
     } while (false)
 
     for (;;) {
@@ -42,12 +90,30 @@ static InterpretResult run() {
                 push(constant);
                 break;
             }
-            case OP_ADD:      BINARY_OP(+); break;
-            case OP_SUBTRACT: BINARY_OP(-); break;
-            case OP_MULTIPLY: BINARY_OP(*); break;
-            case OP_DIVIDE:   BINARY_OP(/); break;
+            case OP_NIL:      push(NIL_VAL); break;
+            case OP_TRUE:     push(BOOL_VAL(true)); break;
+            case OP_FALSE:    push(BOOL_VAL(false)); break;
+            case OP_EQUAL: {
+                Value b = pop();
+                Value a = pop();
+                push(BOOL_VAL(valuesEqual(a, b)));
+                break;
+            }
+            case OP_GREATER:  BINARY_OP(BOOL_VAL, >); break;
+            case OP_LESS:     BINARY_OP(BOOL_VAL, <); break;
+            case OP_ADD:      BINARY_OP(NUMBER_VAL, +); break;
+            case OP_SUBTRACT: BINARY_OP(NUMBER_VAL, -); break;
+            case OP_MULTIPLY: BINARY_OP(NUMBER_VAL, *); break;
+            case OP_DIVIDE:   BINARY_OP(NUMBER_VAL, /); break;
+            case OP_NOT:
+                push(BOOL_VAL(isFalsey(pop())));
+                break;
             case OP_NEGATE: {
-                push(-pop());
+                if (IS_NUMBER(peek(0))) {
+                    runtimeError("Operand must be a number.");
+                    return INTERPRET_RUNTIME_ERROR;
+                }
+                push(NUMBER_VAL(-AS_NUMBER(pop())));
                 break;
             }
             case OP_RETURN: {
@@ -61,13 +127,6 @@ static InterpretResult run() {
 #undef READ_BYTE
 #undef READ_CONSTANT
 #undef BINARY_OP
-}
-
-/**
- * Resets the stack.
- */
-static void resetStack() {
-    vm.stackTop = vm.stack;
 }
 
 /* ===== End static functions ===== */
