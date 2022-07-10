4a3795f176d6a8d6a51a6e168e9179eea8e2473f
Adam Bruce <adam@adambruce.net>
2022-07-10 15:43:58 +0100

    Fix bug where valid identifiers were classified as keywords.

--- clox/scanner.c
+++ clox/scanner.c
@@ -192,6 +192,8 @@ static TokenType identifierType() {
             break;
         case 'v': return checkKeyword(1, 2, "ar", TOKEN_VAR);
         case 'w': return checkKeyword(1, 4, "hile", TOKEN_WHILE);
+        default:
+            return TOKEN_IDENTIFIER;
     }
 }
 
